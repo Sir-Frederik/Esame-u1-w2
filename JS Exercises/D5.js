2f648959aebbbfc79a5d9fcb428ca96c58173343
Federico Brunetti <brunetti.federico.13@gmail.com>
2025-03-21 17:10:33 +0100

    tutto finito! (fino a es10)

--- JS Exercises/D5.js
+++ JS Exercises/D5.js
@@ -128,3 +128,78 @@ const cars = [
     es. [f, b, e] --> [6, 2, 5]
 */
 const charactersArray = ["g", "n", "u", "z", "d"];
+
+const alphabet = "abcdefghilmnopqrstuvz";
+const positionsArray = [];
+
+for (let i = 0; i < charactersArray.length; i++) {
+  switch (charactersArray[i]) {
+    case "a":
+      positionsArray.push(1);
+      break;
+    case "b":
+      positionsArray.push(2);
+      break;
+    case "c":
+      positionsArray.push(3);
+      break;
+    case "d":
+      positionsArray.push(4);
+      break;
+    case "e":
+      positionsArray.push(5);
+      break;
+    case "f":
+      positionsArray.push(6);
+      break;
+    case "g":
+      positionsArray.push(7);
+      break;
+    case "h":
+      positionsArray.push(8);
+      break;
+    case "i":
+      positionsArray.push(9);
+      break;
+    case "l":
+      positionsArray.push(10);
+      break;
+    case "m":
+      positionsArray.push(11);
+      break;
+    case "n":
+      positionsArray.push(12);
+      break;
+    case "o":
+      positionsArray.push(13);
+      break;
+    case "p":
+      positionsArray.push(14);
+      break;
+    case "q":
+      positionsArray.push(15);
+      break;
+    case "r":
+      positionsArray.push(16);
+      break;
+    case "s":
+      positionsArray.push(17);
+      break;
+    case "t":
+      positionsArray.push(18);
+      break;
+    case "u":
+      positionsArray.push(19);
+      break;
+    case "v":
+      positionsArray.push(20);
+      break;
+    case "z":
+      positionsArray.push(21);
+      break;
+    default:
+      break;
+  }
+}
+
+console.log(positionsArray);
